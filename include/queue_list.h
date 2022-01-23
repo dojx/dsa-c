8c74902357d5bfc499dbef7f36c1b44bdc454e14
Diego Jimenez <dojx56@gmail.com>
2022-01-23 14:18:39 -0600

    added subdirectories and modified Makefile

---
+++ include/queue_list.h
@@ -0,0 +1,18 @@
+#ifndef QUEUE_LIST_H
+#define QUEUE_LIST_H
+
+#include "linked_list.h"
+
+typedef struct Queue {
+    int size;
+    Node* head;
+    Node* tail;
+} Queue;
+
+Queue* create_queue(int x);
+void print_q(Queue *q);
+void push(Queue *q, int x);
+int pop(Queue *q);
+void delete_q(Queue **q);
+
+#endif
